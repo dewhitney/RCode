e8d6763c09c9b9995c0fe63f3e054ffd2b9e8d22
dewhitney <daveewhitney@gmail.com>
2014-01-16 09:57:39 -0800

    Update HW1
    Not elegant, but will work.

---
+++ Biostatistical Computing/HW1.R
@@ -0,0 +1,98 @@
+#Regression based on homoskedastic normal data of equal group sizes.
+do.one = function(b0,b1,n){
+  times = n/10
+  x = sort(rep(seq(from=0.1,to=1.0,by=0.1),times))
+  sims = function(x){return(rnorm(1,mean={b0+b1*x},sd=1))}
+  y = mapply(x, FUN=sims)
+  df = data.frame(x,y)
+  model = with(df, lm(y~x))
+  return(summary(model))
+}
+
+beta0 = c(0L,20L)
+beta1 = c(0L,10L)
+n = c(10L,100L,1000L)
+
+#Had some problems working with the 3D array, so used same seed and stored
+#estimated betas and std errors separately. Revisit this later. -DW
+print(c("b0","b1","n","b0hat","b1hat","coverage"))
+for (i in beta0){
+  for(j in beta1){
+    for (k in n){
+      set.seed(1)
+      many.betas = t(replicate(1000, do.one(b0=i,b1=j,n=k)$coefficients[,1]))
+      many.mean = colSums(many.betas/1000)
+      
+      set.seed(1)
+      many.SEs = t(replicate(1000, do.one(b0=i,b1=j,n=k)$coefficients[,2]))
+      
+      cover.beta1 = mean({{many.betas[,2] - 1.96*many.SEs[,2]} < j } & {j < {many.betas[,2] + 1.96*many.SEs[,2]}})
+      print(c(i,j,k,as.vector(many.mean),cover.beta1))
+    }
+  }
+}
+
+#set.seed(1)
+#many.betas = t(replicate(1000, do.one(beta0[1],beta1[1],n[1])$coefficients[,1]))
+#many.mean = colSums(many.betas/1000)
+
+#set.seed(1)
+#many.SEs = t(replicate(1000, do.one(beta0[1],beta1[1],n[1])$coefficients[,2]))
+
+#cover.beta1 = mean({{many.betas[,2] - many.SEs[,2]} < beta0[1] } & {beta0[1] < {many.betas[,2] + many.SEs[,2]}})
+
+#Regression based on data with uniform errors of equal group sizes.
+do.one = function(b0,b1,n){
+  times = n/10
+  x = sort(rep(seq(from=0.1,to=1.0,by=0.1),times))
+  sims = function(x){return(b0+b1*x+runif(1,-3,3))}
+  y = mapply(x, FUN=sims)
+  df = data.frame(x,y)
+  model = with(df, lm(y~x))
+  return(summary(model))
+}
+
+print(c("b0","b1","n","b0hat","b1hat","coverage"))
+for (i in beta0){
+  for(j in beta1){
+    for (k in n){
+      set.seed(1)
+      many.betas = t(replicate(1000, do.one(b0=i,b1=j,n=k)$coefficients[,1]))
+      many.mean = colSums(many.betas/1000)
+      
+      set.seed(1)
+      many.SEs = t(replicate(1000, do.one(b0=i,b1=j,n=k)$coefficients[,2]))
+      
+      cover.beta1 = mean({{many.betas[,2] - 1.96*many.SEs[,2]} < j } & {j < {many.betas[,2] + 1.96*many.SEs[,2]}})
+      print(c(i,j,k,as.vector(many.mean),cover.beta1))
+    }
+  }
+}
+
+#Regression based on heteroskedastic normal data of equal group sizes.
+do.one = function(b0,b1,n){
+  times = n/10
+  x = sort(rep(seq(from=0.1,to=1.0,by=0.1),times))
+  sims = function(x){return(rnorm(1,mean={b0+b1*x},sd=x))}
+  y = mapply(x, FUN=sims)
+  df = data.frame(x,y)
+  model = with(df, lm(y~x))
+  return(summary(model))
+}
+
+print(c("b0","b1","n","b0hat","b1hat","coverage"))
+for (i in beta0){
+  for(j in beta1){
+    for (k in n){
+      set.seed(1)
+      many.betas = t(replicate(1000, do.one(b0=i,b1=j,n=k)$coefficients[,1]))
+      many.mean = colSums(many.betas/1000)
+      
+      set.seed(1)
+      many.SEs = t(replicate(1000, do.one(b0=i,b1=j,n=k)$coefficients[,2]))
+      
+      cover.beta1 = mean({{many.betas[,2] - 1.96*many.SEs[,2]} < j } & {j < {many.betas[,2] + 1.96*many.SEs[,2]}})
+      print(c(i,j,k,as.vector(many.mean),cover.beta1))
+    }
+  }
+}
