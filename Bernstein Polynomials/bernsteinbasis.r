2f4089da66a8075e04be3e7336283f628a1aecfc
dewhitney <daveewhitney@gmail.com>
2014-02-16 16:11:13 -0800

    Updated Bernstein Polynomial code
    A new function implementing De Casteljau's Algorithm. See
    bernsteinbasis.r for a speed comparison using microbenchmark.

--- Bernstein Polynomials/bernsteinbasis.r
+++ Bernstein Polynomials/bernsteinbasis.r
@@ -34,17 +34,45 @@ Bn = function(f=function(x){1},n){
 
 ## To use the output function, BX, on a vector of values t
 
-#t = seq(0,1,.01)
-#Sin = function(x){sin(2*pi*x)}
-#BSin1 = Bn(f=Sin,n=1)
-#BSin10 = Bn(f=Sin,n=10)
-#BSin100 = Bn(f=Sin,n=100)
-#fit1 = BSin1(t)
-#fit10 = BSin10(t)
-#fit100 = BSin100(t)
-#plot(t,Sin(t),type="l",lwd=3,col="lightgreen",main="Approximating Sin by Bernstein Expansion")
-#points(t,Sin(t),type="l",lwd=3,col="forestgreen")
-#points(t,fit1,type="l",lwd=2,col="red")
-#points(t,fit10,type="l",lwd=2,col="purple")
-#points(t,fit100,type="l",lwd=2,col="blue")
+t = seq(0,1,.01)
+Sin = function(x){sin(2*pi*x)}
+BSin1 = Bn(f=Sin,n=1)
+BSin10 = Bn(f=Sin,n=10)
+BSin100 = Bn(f=Sin,n=100)
+fit1 = BSin1(t)
+fit10 = BSin10(t)
+fit100 = BSin100(t)
+plot(t,Sin(t),type="l",lwd=3,col="lightgreen",main="Approximating Sin by Bernstein Expansion")
+points(t,Sin(t),type="l",lwd=3,col="forestgreen")
+points(t,fit1,type="l",lwd=2,col="red")
+points(t,fit10,type="l",lwd=2,col="purple")
+points(t,fit100,type="l",lwd=2,col="blue")
 
+## de Casteljau's Algorithm for evaluating Bezier curves
+
+deCasteljau = function(f,t,n){
+  pts = seq(0,1,1/n)
+  t0 = c(1-t,t)
+  Beta = f(pts)
+  for (k in 1:n){
+    Beta = cbind(Beta[-(length(Beta))],Beta[-1]) %*% t0
+    #print(Beta)
+  }
+  return(Beta)
+}
+
+#deCasteljau(Sin,.6,10)
+
+fastB = function(f,n){
+  B = function(t){return(deCasteljau(f=f,t,n=n))}
+  return(function(t){apply(as.matrix(t),1,B)})
+}
+
+fastBSin100 = fastB(Sin,100)
+
+# library(microbenchmark)
+# microbenchmark(BSin100(t),fastBSin100(t))
+## Unit: milliseconds
+## expr       min        lq    median       uq       max neval
+## BSin100(t) 1213.9194 1233.5991 1247.0700 1289.011 1944.6590   100
+## fastBSin100(t)  309.1887  316.9562  322.6283  342.625  564.1454   100
